0785f87daf625dab063d2d2cf73e9e7f987e3d16
Sam Smith <sams96@mail.com>
2025-08-11 22:23:52 +0200

    add configurability
    fixes #1

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	ffsyncclient v0.0.0-00010101000000-000000000000
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/feeds v1.2.0
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/lestrrat-go/jwx/v3 v3.0.8
 	golang.org/x/crypto v0.39.0
 )
--- main.go
+++ main.go
@@ -10,10 +10,25 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/gorilla/feeds"
+	"github.com/kelseyhightower/envconfig"
 	"github.com/sams96/bookmark-feeder/sync"
 )
 
-func handleGet(sc *sync.SyncClient) http.Handler {
+type config struct {
+	SyncEmail    string `envconfig:"SYNC_EMAIL"`
+	SyncPassword string `envconfig:"SYNC_PASSWORD"`
+
+	ServerAddress string `envconfig:"SERVER_ADDRESS" default:":19928"`
+
+	FeedTitle       string `envconfig:"FEED_TITLE" default:"Bookmarks Feed"`
+	FeedLink        string `envconfig:"FEED_LINK"`
+	FeedDescription string `envconfig:"FEED_DESC" default:"A collection of bookmarks exported to Atom feed format."`
+	FeedAuthor      string `envconfig:"FEED_AUTHOR" default:"Bookmark Feeder"`
+
+	BookmarkFolder string `envconfig:"BOOKMARK_FOLDER"`
+}
+
+func handleGet(sc *sync.SyncClient, config config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		bookmarks, err := sc.GetBookmarks()
 		if err != nil {
@@ -22,13 +37,15 @@ func handleGet(sc *sync.SyncClient) http.Handler {
 			return
 		}
 
-		bookmarks = sync.FilterBookmarks(bookmarks, "Reading List")
+		if config.BookmarkFolder != "" {
+			bookmarks = sync.FilterBookmarks(bookmarks, config.BookmarkFolder)
+		}
 
 		feed := &feeds.Feed{
-			Title:       "Exported Bookmarks Feed",
-			Link:        &feeds.Link{Href: "http://example.com/bookmarks.atom"},
-			Description: "A collection of bookmarks exported to Atom feed format.",
-			Author:      &feeds.Author{Name: "Go Bookmark Exporter"},
+			Title:       config.FeedTitle,
+			Link:        &feeds.Link{Href: config.FeedLink},
+			Description: config.FeedDescription,
+			Author:      &feeds.Author{Name: config.FeedAuthor},
 			Id:          "urn:uuid:" + uuid.NewString(),
 		}
 
@@ -56,16 +73,20 @@ func handleGet(sc *sync.SyncClient) http.Handler {
 }
 
 func main() {
+	var config config
+	err := envconfig.Process("BOOKMARK_FEEDER", &config)
+	if err != nil {
+		slog.Error("Failed to load config", "error", err)
+		os.Exit(1)
+	}
+
 	sc, err := sync.NewSyncClient("./firefox-sync-session.json")
 	if err != nil {
 		slog.Error("Failed to create new SyncClient", "error", err)
 		os.Exit(1)
 	}
 
-	email := os.Getenv("BOOKMARK_FEEDER_SYNC_EMAIL")
-	password := os.Getenv("BOOKMARK_FEEDER_SYNC_PASSWORD")
-
-	err = sc.Login(email, password)
+	err = sc.Login(config.SyncEmail, config.SyncPassword)
 	if err != nil {
 		slog.Error("An error occurred during authentication", "error", err)
 		os.Exit(1)
@@ -74,8 +95,8 @@ func main() {
 	fmt.Println("Successfully logged in and authenticated!")
 
 	s := &http.Server{
-		Addr:           ":19928",
-		Handler:        handleGet(sc),
+		Addr:           config.ServerAddress,
+		Handler:        handleGet(sc, config),
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
